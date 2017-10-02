b71ace3e3d14a0c0d3dfbdcb3fdc261bf8ba4f38
Boštjan Zupančič <32441668+6ftunder@users.noreply.github.com>
2017-10-02 18:14:45 +0200

    changed to solution with len, added comments

--- py/Aaah!/aaah.py
+++ py/Aaah!/aaah.py
@@ -1,7 +1,5 @@
-jon = input()  # jon's aaah
-required = input()  # required aaah
+# count method
+# print('go' if input().count('a') >= input().count('a') else 'no') # if number of a's in john is >= than required print 'go' else 'no'
 
-if jon.count('a') >= required.count('a'):
-    print('go')
-else:
-    print('no')
+# if len of john's aah is >= required print 'go' else 'no'
+print('go' if len(input()) >= len(input()) else 'no')
