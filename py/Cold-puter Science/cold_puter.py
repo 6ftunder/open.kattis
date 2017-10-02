fadb16262da19f175a859a6c773f063839504f79
Boštjan Zupančič <32441668+6ftunder@users.noreply.github.com>
2017-10-02 18:33:31 +0200

    improved comments

--- py/Cold-puter Science/cold_puter.py
+++ py/Cold-puter Science/cold_puter.py
@@ -1,9 +1,9 @@
-_ = input()
-temps = map(int, input().split())
+_ = input()  # number of test cases; we can ignore this since it has no significance
+temps = map(int, input().split())  # all the temps stored to a list with type int
 
 subercold = 0  # how many temps below 0 we got
 for i in temps:
-    # for every temp we recorded if it's less than 0 add to the subercold temps
+    # for every temp we recorded if it's less than 0 add +1 to subercold
     if i < 0:
         subercold += 1
 print(subercold)
