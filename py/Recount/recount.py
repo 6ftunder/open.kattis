e310f225226f391b6da690b6641f4a0f7d21b8a7
Boštjan Zupančič <32441668+6ftunder@users.noreply.github.com>
2017-10-04 13:08:21 +0200

    improved comments

--- py/Recount/recount.py
+++ py/Recount/recount.py
@@ -1,19 +1,23 @@
 import sys
 
 # cpu time 0.13 s
-candidates = {}  # dict of all the candidates 'name': votes
+CANDIDATES = {}  # dict of all the candidates 'name': votes
 
 for candidate in sys.stdin:
-    # for every candidate that we put in, we add them to the candidates dict, if already added add +1 to value(votes)
+    # for every candidate that we put in, we add them to the CANDIDATES dict,
+    # if already added add +1 to value(votes)
     # end the loop when we recieve ***
 
     if candidate == '***\n':
         # print out and break if at end '***'
-        winner = max(candidates, key=candidates.get)  # find the winner of the election
-        print(winner.rstrip('\n') if list(candidates.values()).count(
-            candidates[
-                winner]) < 2 else 'Runoff!')  # check if there is more than one winner. print winner's name, else print Runoff!
+
+        # find the winner of the election
+        winner = max(CANDIDATES, key=CANDIDATES.get)
+
+        # check if there is more than one winner. print winner's name, else print Runoff!
+        print(winner.rstrip('\n') if list(CANDIDATES.values()).count(
+            CANDIDATES[winner]) < 2 else 'Runoff!')
         break
 
-    candidates[candidate] = candidates.get(candidate,
-                                           0) + 1  # add candidate if not in; add +1 to the value of candidate
+    # add candidate if not in; add +1 to the value of candidate
+    CANDIDATES[candidate] = CANDIDATES.get(candidate, 0) + 1
