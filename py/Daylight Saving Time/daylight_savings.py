6137732f4985b661d752726b20703d3a21160d7a
Boštjan Zupančič <32441668+6ftunder@users.noreply.github.com>
2017-10-04 13:14:34 +0200

    comments

--- py/Daylight Saving Time/daylight_savings.py
+++ py/Daylight Saving Time/daylight_savings.py
@@ -1,9 +1,17 @@
 import sys
+
+# cpu time 0.2s
 for i in sys.stdin:
     for j in sys.stdin:
-        forwards_backwards, change, hour, minute = map(lambda x: int(x) if x.isdigit() else str(x), j.split())  # splitting the input at ' ' and converting input into int if it's a digit else str
-        time_in_minutes = ((hour*60) + minute) + change if forwards_backwards == 'F' else ((hour * 60) + minute) - change # time added together in minutes, added or removed change in minutes
+        # splitting the input at ' ' and converting input into int if it's a digit else str
+        forwards_backwards, change, hour, minute = map(
+            lambda x: int(x) if x.isdigit() else str(x), j.split())
+
+        # time added together in minutes, added or removed change in minutes
+        time_in_minutes = ((hour * 60) + minute) + \
+            change if forwards_backwards == 'F' else \
+                ((hour * 60) + minute) - change
         # we take hours and get the remainder with 24
         # hours % 24
 
-        print('{} {}'.format(time_in_minutes//60 % 24, time_in_minutes % 60))
+        print('{} {}'.format(time_in_minutes // 60 % 24, time_in_minutes % 60))
