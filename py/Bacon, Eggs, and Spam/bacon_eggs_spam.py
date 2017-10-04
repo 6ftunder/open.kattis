6a7d8feecc4271f7bcb110d50e20c6055f6f532e
Boštjan Zupančič <32441668+6ftunder@users.noreply.github.com>
2017-10-04 15:29:29 +0200

    added problems

---
+++ py/Bacon, Eggs, and Spam/bacon_eggs_spam.py
@@ -0,0 +1,32 @@
+import sys
+
+for i in sys.stdin:
+    # input of i-inputs
+
+    if i.rstrip() == '0':
+        # reched the end break the loop
+        break
+    menu = dict()
+
+    for _ in range(int(i)):
+        # go through the inputs i times add them to menu dict
+        order = input().rstrip().split()
+
+        for thing in order[1:]:
+            # for every thing in the order add to dict as key,
+            # add person to value of food/drink ordered
+
+            if not menu.get(thing, False):
+                # if we don't have the item in the menu add it in
+                menu[thing] = []
+
+            # add the person who ordered it to the list of people that
+            # ordered the item
+            menu[thing] += [order[0]]
+
+    for key, value in sorted(menu.items()):
+        # for every sorted key
+        # print out ordered keys with sorted values as string
+        print(key, ' '.join(sorted(menu[key])) + ' ')
+
+    print()  # print empty line
