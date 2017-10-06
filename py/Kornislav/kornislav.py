7ea70f919f7a053d44deb0329351415abd9242b4
Boštjan Zupančič <32441668+6ftunder@users.noreply.github.com>
2017-10-06 21:03:27 +0200

    added problems

---
+++ py/Kornislav/kornislav.py
@@ -0,0 +1,4 @@
+# to get the biggest rectangle the turtle can make
+#  we sort the inputs and multiply first and third number
+inputs = sorted(input().split())
+print('{}'.format((int(inputs[0])*int(inputs[2]))))
