af89f410183fc67c6084d6a454b82662eae83cab
Boštjan Zupančič <32441668+6ftunder@users.noreply.github.com>
2017-10-06 15:12:30 +0200

    final solution 0.8s

--- py/Image Decoding/image_decoding.py
+++ py/Image Decoding/image_decoding.py
@@ -1,40 +1,37 @@
-import sys
 # work in progress,
 # working version will be merged
 # to master branch
 #
 #
 #
-def output(list):
-    # prints out decoded images in the list
-    for line in list[1:-1]:
-        print(line if line else '')
-    print(list[-1], end="")
-
 def first_line(line):
     # get the first input and set the needed length of lines
-    current_pixel, number_of_outputs = line[0], line[1:]
+    pixel, number_of_outputs = line[0], line[1:]
     line_output = ''  # the current line
     for times in number_of_outputs:
-        line_output += current_pixel * times  # add the current pixels in the line
-        current_pixel = d[current_pixel]  # change into second pixel
+        line_output += pixel * times  # add the current pixels in the line
+        pixel = d[pixel]  # change into second pixel
+    print(line_output)
 
-    return len(line_output), line_output
+    return sum(number_of_outputs)
 
 d = {'.': '#', '#': '.'}  # next pixel we have to take
-decoded_output = []  # all the output we will print
-for i in sys.stdin:
-    if i == '0\n':
-        # if at end print out
-        output(decoded_output)
+first = True  # we have not printed anything yet, so skip the first \n
+while True:
+    i = int(input())
+    if i == 0:
+        # if at end break
         break
 
-    decoding_error = False  # if we detect an error in our output (more pixels than needed
-    decoded_output.append('')
+    if first:
+        first = not first
+    else:
+        print()
+
+    decoding_error = False  # if we detect an error in our output (more pixels than needed)
     first_line_return = first_line(list(map(lambda x: int(x) if x.isdigit() else x, input().split())))
-    length_of_lines = first_line_return[0]
-    decoded_output.append(first_line_return[1])
-    for _ in range(int(i) - 1):
+    length_of_lines = first_line_return
+    for _ in range(i-1):
         # for every input in the range provided decode the image
         current_input = list(map(lambda x: int(x) if x.isdigit() else x, input().split()))
         current_pixel, number_of_outputs = current_input[0], current_input[1:]
@@ -42,8 +39,10 @@ for i in sys.stdin:
         for times in number_of_outputs:
             line_output += current_pixel * times  # add the current pixels in the line
             current_pixel = d[current_pixel]  # change into second pixel
-        decoded_output.append(line_output)
+        print(line_output)
         if not decoding_error:
-            if length_of_lines < len(line_output):
-                decoded_output.append('Error decoding image')
+            if length_of_lines != sum(number_of_outputs):
                 decoding_error = not decoding_error
+
+    if decoding_error:
+        print('Error decoding image')
